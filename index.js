5323b61fe43b0aba4bbfe443b3c35eb68659265c
Rohit Kumar <rohitraj72342@gmail.com>
2024-02-17 18:32:10 +0530

    cors updated

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParse());
 app.use(
     cors({
-        origin: "https://studynotion-azure.vercel.app/",
+        origin: "https://studynotion-azure.vercel.app",
         credentials: true
     })
 );
